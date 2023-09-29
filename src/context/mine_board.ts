ca1a9c291595ec94b9dcb38dcd65ef9fc229e605
jayme.mckiney <jayme.mckiney@gmail.com>
2023-09-29 13:10:49 -0700

    created model controller for the minesweeper board state

---
+++ src/context/mine_board.ts
@@ -0,0 +1,165 @@
+import {Grid, Coord} from "./grid.ts"
+
+export type TileState = number | "bomb"
+export type MaskState = "fog" | "clear" | "flag"
+export type GameState = "active" | "boom" | "win"
+export type Color = "white" | "green" | "yellow" | "orange" | "red"
+export interface TileInfo {
+  value: TileState | MaskState,
+  color: Color,
+  coord: Coord
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max)
+}
+function colorFromValue(value:number): Color {
+  const colors: Array<Color> = ["white", "green", "yellow", "orange"]
+  if(value >= colors.length) {
+    return colors[colors.length -1]
+  }
+  return colors[value]
+}
+
+export class MineBoard {
+  #width: number;
+  #height: number;
+  #bombCount: number;
+  #flagged: number;
+  #gameState: GameState;
+  #tiles: Grid<TileState>;
+  #mask: Grid<MaskState>;
+  constructor(width: number, height: number, bombCount: number, initialTileState: Array<TileState> = []) {
+    this.#width = width
+    this.#height = height
+    this.#bombCount = bombCount
+    this.#gameState = "active"
+    this.#flagged = 0
+    this.#mask = new Grid<MaskState>(this.#width, this.#height, "fog")
+    this.#tiles = new Grid<TileState>(this.#width, this.#height, 0)
+    if(initialTileState.length == 0) {
+      this.#plantBombs()
+    } else {
+      // this exists for testing purposes
+      let i = 0
+      for(let coord of this.#tiles.iterateCoords()) {
+        this.#tiles.set(coord, initialTileState[i])
+        i ++
+      }
+    }
+    this.#numberTiles()
+  }
+  #plantBombs() {
+    let coords = Array<Coord>()
+    for(let coord of this.#tiles.iterateCoords()) {
+      coords.push(coord)
+    }
+    for(let i = 0; i < this.#bombCount; i++) {
+      let index = getRandomInt(coords.length)
+      this.#tiles.set(coords[index], "bomb")
+      coords.splice(index, 1)
+    }
+  }
+  #numberTiles() {
+    for(let coord of this.#tiles.iterateCoords()) {
+      if(this.#tiles.get(coord) == "bomb")
+        continue
+      let neighborBombs = 0
+      for(let neighborCoord of this.#tiles.iterateNeighborCoords(coord)) {
+        if(this.#tiles.get(neighborCoord) == "bomb")
+          neighborBombs ++
+      }
+      this.#tiles.set(coord, neighborBombs)
+    }
+  }
+  clearFog() {
+    for(let coord of this.#tiles.iterateCoords()) {
+      if(this.#mask.get(coord) == "fog") {
+        this.#mask.set(coord, "clear")
+        if(this.#tiles.get(coord) == "bomb") {
+          this.#gameState = "boom"
+        }
+      }
+    }
+    this.gameStateCheck()
+  }
+  gameStateCheck() {
+    if(this.#gameState == 'boom' || this.#flagged != this.#bombCount) {
+      return
+    }
+    for(let coord of this.#mask.iterateCoords()) {
+      if(this.#mask.get(coord) == 'fog') {
+        return
+      }
+    }
+    this.#gameState = "win"
+  }
+  gameState(): GameState {
+    return this.#gameState
+  }
+  flagCoord(coord:Coord) {
+    if(this.#mask.get(coord) == 'fog') {
+      this.#mask.set(coord, 'flag')
+      this.#flagged ++
+    } else if(this.#mask.get(coord) == "flag") {
+      this.#mask.set(coord, "fog")
+      this.#flagged --
+    }
+  }
+  checkCoord(coord: Coord) {
+    if(this.#mask.get(coord) == 'fog') {
+      this.#mask.set(coord, 'clear')
+      if(this.#tiles.get(coord) == "bomb") {
+        this.#gameState = "boom"
+      } else if(this.#tiles.get(coord) == 0) {
+        this.#colapseNeighbors(coord)
+      }
+      this.gameStateCheck()
+    }
+  }
+  #colapseNeighbors(coord:Coord) {
+    for(let neighborCoord of this.#tiles.iterateNeighborCoords(coord)) {
+      if(this.#mask.get(neighborCoord) != "fog") {
+        continue
+      }
+      this.#mask.set(neighborCoord, "clear")
+      if(this.#tiles.get(neighborCoord) == 0) {
+        this.#colapseNeighbors(neighborCoord)
+      }
+    }
+  }
+  *iterateRows(): IterableIterator<Array<TileInfo>> {
+    let curRowCount = 0
+    let row = Array<TileInfo>()
+    for(let coord of this.#tiles.iterateCoords()) {
+      if(curRowCount >= this.#width) {
+        yield row
+        curRowCount = 0
+        row = Array<TileInfo>()
+      }
+      let maskState: MaskState = this.#mask.get(coord)
+      let info: TileInfo
+      let color:Color = "red"
+      if(maskState == "clear") {
+        let value = this.#tiles.get(coord)
+        if(value != "bomb") {
+          color = colorFromValue(value)
+        }
+        info = {
+          value: value,
+          color: color,
+          coord: coord
+        }
+      } else {
+        info = {
+          value: maskState,
+          color: color,
+          coord: coord
+        }
+      }
+      row.push(info)
+      curRowCount ++
+    }
+    yield row
+  }
+}
